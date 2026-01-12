65bace4631e7e3441e00e4d269bec8e15fa5e3c8
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2026-01-12 11:02:06 +0100

    la création et l'implémentation du fichier auth.service.ts

--- src/app/core/models/user.model.ts
+++ src/app/core/models/user.model.ts
@@ -8,3 +8,33 @@ export interface User {
   active: boolean;
   createdAt: Date;
 }
+
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  token: string;
+}
+
+export interface OAuth2LoginResponse {
+  accessToken: string;
+}
+
+export interface UserRequestDTO {
+  email: string;
+  password: string;
+  fullName: string;
+  role?: Role;
+}
+
+export interface UserResponseDTO {
+  id: number;
+  email: string;
+  fullName: string;
+  role: Role;
+  active: boolean;
+  createdAt: Date;
+}
