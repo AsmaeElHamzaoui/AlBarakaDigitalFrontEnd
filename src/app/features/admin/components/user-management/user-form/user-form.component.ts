1ee76f5cc5f0c088d876a324bd6307fcc5e3aa33
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2026-01-15 15:06:26 +0100

    la création du composant user form component

---
+++ src/app/features/admin/components/user-management/user-form/user-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-form',
+  standalone: true,
+  template: `
+    <h2>Création utilisateur</h2>
+    <p>Fonctionnalité à implémenter (optionnelle)</p>
+  `
+})
+export class UserFormComponent {}
