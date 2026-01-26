bdc3067df92cebdafc27121cebd20072078239e5
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2026-01-26 09:59:51 +0100

    update guards and services

--- src/app/core/guards/role.guard.ts
+++ src/app/core/guards/role.guard.ts
@@ -1,13 +1,13 @@
 import { inject } from '@angular/core';
-import { Router, CanActivateFn } from '@angular/router';
+import { Router, CanActivateFn, ActivatedRouteSnapshot } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { Role } from '../models/role.enum';
 
-export const roleGuard: CanActivateFn = (route, state) => {
+export const roleGuard: CanActivateFn = (route: ActivatedRouteSnapshot) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  const requiredRole = route.data['role'] as Role;
+  const expectedRoles: Role[] = route.data['roles'] as Role[];
   const userRole = authService.getUserRole();
 
   if (!userRole) {
@@ -15,14 +15,12 @@ export const roleGuard: CanActivateFn = (route, state) => {
     return false;
   }
 
-  if (userRole === requiredRole) {
+  if (expectedRoles.includes(userRole)) {
     return true;
   }
 
   // Rediriger vers le dashboard approprié si mauvais rôle
-  console.warn(`Accès refusé. Rôle requis: ${requiredRole}, Rôle utilisateur: ${userRole}`);
+  console.warn(`Accès refusé. Rôle requis: ${expectedRoles}, Rôle utilisateur: ${userRole}`);
   authService.redirectByRole();
   return false;
-
-  
 };
